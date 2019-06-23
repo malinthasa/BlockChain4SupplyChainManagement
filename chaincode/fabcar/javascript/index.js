c8ae85ccb643960d213a94f76f73af4185f7a9a3
malintha <malinthasa@gmail.com>
2019-06-23 05:13:17 +0200

    interm:

---
+++ chaincode/fabcar/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Fabcar = require('./lib/fabcar');
+
+module.exports.Fabcar = Fabcar;
+module.exports.contracts = [ Fabcar ];
